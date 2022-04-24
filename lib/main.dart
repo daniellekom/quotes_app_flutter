0bfd81a10ecf985b7fa656976e9c0cf3a3ee8bb8
Danielle Kom <daniellekom1@gmail.com>
2022-04-24 16:07:53 -0400

    created delete stateless widget for quotes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(MaterialApp(
   home:QuoteList(),
@@ -20,34 +21,6 @@ Quote(author: 'Oscar wild', text: 'I have nothing to declare but my genius'),
 Quote(author: 'Oscar wild', text: 'The truth is rarely true and never simple'),
   ];
 
-Widget quoteTemplate(quote){
-  return Card(
-    margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-    child:Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-        Text(
-          quote.text,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Colors.grey[600],
-          ),
-        ),
-        const SizedBox(height: 6.0),
-        Text(
-          quote.author,
-          style:TextStyle(
-            fontSize: 14.0,
-            color:Colors.grey[400],
-          )
-        )
-      ],),
-    )
-  );
-}
-  
 
   @override
   Widget build(BuildContext context) {
@@ -59,9 +32,17 @@ Widget quoteTemplate(quote){
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote)=> quoteTemplate(quote)).toList(),
+        children: quotes.map((quote)=>QuoteCard(
+          quote: quote,
+          delete: () {
+            setState((){
+              quotes.remove(quote);
+            });
+          }
+        )).toList(),
       ) 
 
     );
   }
 }
+
